8c3cb7ff1addd3dc3b28d2441136d4b33b0ca0c8
Max <maximtsyba@gmail.com>
2026-04-24 23:46:11 +0200

    Refactor background code

--- background.js
+++ background.js
@@ -1,8 +1,9 @@
 chrome.action.onClicked.addListener(() => switchToLastTab());
-chrome.commands.onCommand.addListener((command, senderTab) => {
+chrome.commands.onCommand.addListener((command) => {
     if (command === "switch-to-last-tab") switchToLastTab();
 })
 chrome.tabs.onRemoved.addListener(onTabClose);
+chrome.tabs.onActivated.addListener(onTabActivated);
 
 function switchToLastTab(updateIcon = true){
     chrome.tabs.query({ currentWindow: true, active: false }, (tabs) => {
@@ -12,28 +13,20 @@ function switchToLastTab(updateIcon = true){
 
         if (previousTab?.id) {
             if (updateIcon) chrome.tabs.onActivated.removeListener(onTabActivated);
-
             chrome.tabs.update(previousTab.id, { active: true }, function(){
-                if (updateIcon) chrome.tabs.onActivated.addListener(onTabActivated);
-            });
+                if (updateIcon) {
+                    chrome.tabs.onActivated.addListener(onTabActivated);
+                    chrome.storage.session.set({ lastActiveTabId: previousTab.id });
 
-            if (updateIcon){
-                const currentTitle = chrome.action.getTitle({}, (title) => {
-                    if (title === "Switch to Last Tab") {
-                        chrome.action.setTitle({ title: "Switch Back to Last Tab" });
-                        // chrome.action.setIcon({ path: "icon-reverse.png"});
-                        // chrome.action.setBadgeText({ text: "⮣" });
-                        // chrome.action.setBadgeText({ text: "→" });
-                        chrome.action.setBadgeText({ text: "🡢" });
-                        chrome.action.setBadgeBackgroundColor({ color: "rgb(102, 181, 54)" });
-                        chrome.action.setBadgeTextColor({ color: "white" });
-                    } else {
-                        chrome.action.setTitle({ title: "Switch to Last Tab" });
-                        // chrome.action.setIcon({ path: "icon.png"});
-                        chrome.action.setBadgeText({ text: "" });
-                    }
-                });
-            }
+                    chrome.action.getBadgeText({}, (text) => {
+                        if (text === "") {
+                            setIcon(true);
+                        } else {
+                            setIcon(false);
+                        }
+                    });
+                }
+            });
         } 
     });
 }
@@ -41,16 +34,32 @@ function switchToLastTab(updateIcon = true){
 function onTabActivated(activeInfo) {
     chrome.windows.get(activeInfo.windowId, { populate: false }, (window) => {
         if (window && window.type === "normal") {
-            // chrome.action.setIcon({ path: "icon.png"});
-            chrome.action.setTitle({ title: "Switch to Last Tab" });
-            chrome.action.setBadgeText({ text: "" });
-            chrome.tabs.onActivated.removeListener(onTabActivated)
+            setIcon(false);
         }
+
+        /// delayed by windows.get to avoid race condition with onTabClose
+        chrome.storage.session.set({ lastActiveTabId: activeInfo.tabId });
     });
 }
 
 function onTabClose(tabId, removeInfo) {
-    if (!removeInfo || !removeInfo.isWindowClosing) {
-        switchToLastTab(false);
+    if (removeInfo && removeInfo.isWindowClosing) return;
+
+    chrome.storage.session.get("lastActiveTabId", (data) => {
+        if (data.lastActiveTabId === tabId) {
+            switchToLastTab(false);
+        }
+    });
+}
+
+function setIcon(showBadge = true){
+    if (showBadge) {
+        chrome.action.setTitle({ title: "Switch Back to Last Tab" });
+        chrome.action.setBadgeText({ text: "🡢" });
+        chrome.action.setBadgeBackgroundColor({ color: "rgb(102, 181, 54)" });
+        chrome.action.setBadgeTextColor({ color: "white" });
+    } else {
+        chrome.action.setTitle({ title: "Switch to Last Tab" });
+        chrome.action.setBadgeText({ text: "" });
     }
 }
