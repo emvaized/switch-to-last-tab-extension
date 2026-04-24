ce65562ece67a47585550b2b6a20a80b795c76f7
Max <maximtsyba@gmail.com>
2026-04-24 22:51:49 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# <img src="icon.png" height="27px"/> Switch to Last Tab
+
+Fast and lightweight extension which allows to quickly switch to last accessed tab. 
+
+- No complicated code for tab tracking, just pure and straightforward sorting on each call
+- Works right after browser restart
+- Adds a toolbar button which switches to last tab on click
+- Shows a small badge when switched to previous tab
+- Provides a keyboard hotkey (default: <kbd>Alt</kbd>+<kbd>Q</kbd>, can be changed)
+- Focuses last active tab on tab close
+
+There are many extensions that do the same, but they use complex code, storing their own history of tabs. There's no need to reinvent the wheel, when your browser already does that.
+This extension uses minimal code and only native functions to achieve the same result. 
+
+This extension is based on [Last Tab Button](https://github.com/pruna00/lasttabbtn), which I adapted for Chrome, updated icon, optimized code and added few features.
---
+++ background.js
@@ -0,0 +1,54 @@
+chrome.action.onClicked.addListener(() => switchToLastTab());
+chrome.commands.onCommand.addListener((command, senderTab) => {
+    if (command === "switch-to-last-tab") switchToLastTab();
+})
+chrome.tabs.onRemoved.addListener(onTabClose);
+
+function switchToLastTab(updateIcon = true){
+    chrome.tabs.query({ currentWindow: true, active: false }, (tabs) => {
+        if (tabs.length < 2) return;
+        const previousTab = tabs
+            .reduce((latest, current) => current.lastAccessed > latest.lastAccessed ? current : latest);
+
+        if (previousTab?.id) {
+            if (updateIcon) chrome.tabs.onActivated.removeListener(onTabActivated);
+
+            chrome.tabs.update(previousTab.id, { active: true }, function(){
+                if (updateIcon) chrome.tabs.onActivated.addListener(onTabActivated);
+            });
+
+            if (updateIcon){
+                const currentTitle = chrome.action.getTitle({}, (title) => {
+                    if (title === "Switch to Last Tab") {
+                        chrome.action.setTitle({ title: "Switch Back to Last Tab" });
+                        // chrome.action.setIcon({ path: "icon-reverse.png"});
+                        chrome.action.setBadgeText({ text: "⮣" });
+                        chrome.action.setBadgeBackgroundColor({ color: "rgb(102, 181, 54)" });
+                        chrome.action.setBadgeTextColor({ color: "white" });
+                    } else {
+                        chrome.action.setTitle({ title: "Switch to Last Tab" });
+                        // chrome.action.setIcon({ path: "icon.png"});
+                        chrome.action.setBadgeText({ text: "" });
+                    }
+                });
+            }
+        } 
+    });
+}
+
+function onTabActivated(activeInfo) {
+    chrome.windows.get(activeInfo.windowId, { populate: false }, (window) => {
+        if (window && window.type === "normal") {
+            // chrome.action.setIcon({ path: "icon.png"});
+            chrome.action.setTitle({ title: "Switch to Last Tab" });
+            chrome.action.setBadgeText({ text: "" });
+            chrome.tabs.onActivated.removeListener(onTabActivated)
+        }
+    });
+}
+
+function onTabClose(tabId, removeInfo) {
+    if (!removeInfo || !removeInfo.isWindowClosing) {
+        switchToLastTab(false);
+    }
+}
